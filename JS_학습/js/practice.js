e44f53ca7a416554fa8580272c2d21c6ae14c86d
inseoung <thonam123@naver.com>
2024-03-25 14:50:16 +0900

    임시저장

---
+++ JS_학습/js/practice.js
@@ -0,0 +1,47 @@
+/* 13번 문제
+x = 14
+if ((10 < x) && (x < 20)) {
+  console.log(x);
+} */
+
+/* 14번 문제
+ i = 1;
+ while (i <=10) {
+  if(i % 5 == 0) {
+    console.log(i);
+  }
+  i++;
+} */
+
+/* 15번 문제
+for(let i = 10; i >= 0; i -= 3) {
+  console.log(i);
+} */
+
+/* 16번 문제
+function traf(x) {
+  traf1 = x * 100/254;
+  traf2 = traf1 * 1/12;
+  console.log(traf1 + "inch");
+  console.log(traf2 + "feet");
+}
+console.log(traf(100)); */
+
+/* 17번 문제
+const pie = 3.14; 
+function value(x, y) {
+  console.log("부피는 " + x * x * pie * y +"cm^2 입니다.")
+};
+console.log(value(5, 10)); */
+
+// 18번 문제
+
+
+// 19번 문제
+
+// 20번 문제
+
+// 21번 문제
+
+// 22번 문제
+
