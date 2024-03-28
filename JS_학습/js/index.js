896956302bf7250b15f6e71c1bf3d1c793a72de4
inseoung <thonam123@naver.com>
2024-03-28 18:56:18 +0900

    Chapter11_JQuery완료
    jQuery부분 완료

---
+++ JS_학습/js/index.js
@@ -0,0 +1,7 @@
+console.log('-----------------')
+// 다항 연산자
+// and 일 때 왼쪽이 false면 왼쪽 값, or 일 때 왼쪽이 false면 오른쪽 값 
+console.log(true || '아이브');
+console.log(false || '아이브');
+console.log(true && '아이브');
+console.log(false && '아이브');
